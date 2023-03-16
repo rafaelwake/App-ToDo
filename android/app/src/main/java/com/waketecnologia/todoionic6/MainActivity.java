3cda60b07e9a58d26bef545dbb6f32c935ed69bd
Rafael Bernardes <60991402+rafaelwake@users.noreply.github.com>
2023-03-16 16:46:07 -0300

    finish app to publish at store

---
+++ android/app/src/main/java/com/waketecnologia/todoionic6/MainActivity.java
@@ -0,0 +1,5 @@
+package com.waketecnologia.todoionic6;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.waketecnologia.todoionic6',
   appName: 'todo',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
